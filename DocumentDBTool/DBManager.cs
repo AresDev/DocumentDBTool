1bc2f1bf7894dfbc8950be6d8ed34684a2e270df
Armando Restrepo <armando.restrepo@gmail.com>
2015-11-11 11:13:16 -0500

    First draft

---
+++ DocumentDBTool/DBManager.cs
@@ -0,0 +1,157 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DBManager.cs" company="Intergrupo S.A.">
+//    Copyright (c) Intergrupo S.A. Todos los Derechos Reservados   
+//    La información aquí contenida es propietaria y confidencial.            
+// </copyright>
+//-----------------------------------------------------------------------
+namespace DocumentDBTool
+{
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Microsoft.Azure.Documents.Linq;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// DB Manager
+    /// </summary>
+    public static class DBManager
+    {
+        /// <summary>
+        /// database Id
+        /// </summary>
+        private static readonly string DatabaseId = DocumentDBTool.Properties.Settings.Default.Database;
+
+        /// <summary>
+        /// collection Id
+        /// </summary>
+        private static readonly string CollectionId = DocumentDBTool.Properties.Settings.Default.Collection;
+
+        /// <summary>
+        /// end point Url
+        /// </summary>
+        private static readonly string EndpointUrl = DocumentDBTool.Properties.Settings.Default.Url;
+
+        /// <summary>
+        /// authorization Key
+        /// </summary>
+        private static readonly string AuthorizationKey = DocumentDBTool.Properties.Settings.Default.Key;
+
+        /// <summary>
+        /// the collection
+        /// </summary>
+        private static Task<DocumentCollection> collection;
+
+        /// <summary>
+        /// the client
+        /// </summary>
+        private static DocumentClient client;
+
+        /// <summary>
+        /// the database
+        /// </summary>
+        private static Database database;
+
+        /// <summary>
+        /// Gets or sets the Collection
+        /// </summary>
+        public static Task<DocumentCollection> Collection
+        {
+            get
+            {
+                if (DBManager.collection == null)
+                {
+                    DBManager.collection = GetOrCreateCollectionAsync(DBManager.Database.SelfLink, CollectionId);
+                }
+
+                return DBManager.collection;
+            }
+
+            set
+            {
+                DBManager.collection = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Client
+        /// </summary>
+        public static DocumentClient Client
+        {
+            get
+            {
+                if (DBManager.client == null)
+                {
+                    var policy = new ConnectionPolicy
+                    {
+                        ConnectionMode = ConnectionMode.Direct,
+                        ConnectionProtocol = Protocol.Tcp
+                    };
+                    DBManager.client = new DocumentClient(new Uri(EndpointUrl), AuthorizationKey, policy);
+                }
+
+                return DBManager.client;
+            }
+
+            set
+            {
+                DBManager.client = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the database
+        /// </summary>
+        public static Database Database
+        {
+            get
+            {
+                if (DBManager.database == null)
+                {
+                    DBManager.database = GetOrCreateDatabaseAsync(DatabaseId).Result;
+                }
+
+                return DBManager.database;
+            }
+
+            set
+            {
+                DBManager.database = value;
+            }
+        }
+
+        /// <summary>
+        /// Get a DocumentCollection by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="databaseLink">The Database SelfLink property where this DocumentCollection exists / will be created</param>
+        /// <param name="id">The id of the DocumentCollection to search for, or create.</param>
+        /// <returns>The matched, or created, DocumentCollection object</returns>
+        private static async Task<DocumentCollection> GetOrCreateCollectionAsync(string databaseLink, string id)
+        {
+            DocumentCollection collection = Client.CreateDocumentCollectionQuery(databaseLink).Where(c => c.Id == id).ToArray().FirstOrDefault();
+            if (collection == null)
+            {
+                collection = await client.CreateDocumentCollectionAsync(databaseLink, new DocumentCollection { Id = id });
+            }
+
+            return collection;
+        }
+
+        /// <summary>
+        /// Get a Database by id, or create a new one if one with the id provided doesn't exist.
+        /// </summary>
+        /// <param name="id">The id of the Database to search for, or create.</param>
+        /// <returns>The matched, or created, Database object</returns>
+        private static async Task<Database> GetOrCreateDatabaseAsync(string id)
+        {
+            Database database = Client.CreateDatabaseQuery().Where(db => db.Id == id).ToArray().FirstOrDefault();
+            if (database == null)
+            {
+                database = await Client.CreateDatabaseAsync(new Database { Id = id });
+            }
+
+            return database;
+        }
+    }
+}
---
+++ DocumentDBTool/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocumentDBTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Consultar_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() != string.Empty)
+            {
+                textBox2.Text = GetDocument(textBox1.Text.Trim()).ToString();
+            }
+        }
+
+        private Document GetDocument(string documentId)
+        {
+            Uri docURI = UriFactory.CreateDocumentUri(DBManager.Database.ToString(), DBManager.Collection.ToString(), textBox1.Text);
+            Uri collectionURI = UriFactory.CreateDocumentCollectionUri(DocumentDBTool.Properties.Settings.Default.Database, DocumentDBTool.Properties.Settings.Default.Collection);
+            var document = DBManager.Client.CreateDocumentQuery<Document>(collectionURI)
+                .Where(doc => doc.Id == documentId)
+                .AsEnumerable()
+                .Single();
+            return document;
+        }
+
+        private void Actualizar_Click(object sender, EventArgs e)
+        {
+            Uri collectionURI = UriFactory.CreateDocumentCollectionUri(DocumentDBTool.Properties.Settings.Default.Database, DocumentDBTool.Properties.Settings.Default.Collection);
+            var result = DBManager.Client.UpsertDocumentAsync(collectionURI, JsonConvert.DeserializeObject(textBox2.Text)).Result;
+            if (result != null)
+            {
+                textBox2.Clear();
+                textBox2.Text = GetDocument(textBox1.Text.Trim()).ToString();
+                MessageBox.Show("Actualizado correctamente");
+            }
+            else
+            {
+                MessageBox.Show("Ha ocurrido un error, por favor intente nuevamente");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
